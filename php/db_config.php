e6238d8250f3b81a006a57d2aa8bcc46911a9da0
lebigted <103762641+lebigted@users.noreply.github.com>
2024-09-27 19:08:11 +0200

    config

--- php/db_config.php
+++ php/db_config.php
@@ -1,22 +1,27 @@
 <?php
-// db_config.php
 
-$host = 'localhost';
-$db   = 'ecf';
-$user = 'root';
-$pass = 'Lulu150304@@';
+// Extraire les informations depuis l'URL
+$dbopts = parse_url(getenv('ddb0g16nfr7a82'));
+
+$host = $dbopts["clhtb6lu92mj2.cluster-czz5s0kz4scl.eu-west-1.rds.amazonaws.com"];
+$db   = ltrim($dbopts["path"], '/');
+$user = $dbopts["u8ut1p2g8ec5em"];
+$pass = $dbopts["p2471cc826ad4d4f27dd1743e1c21593dcd42deb931f36b4740e931266602b07f"];
+$port = $dbopts["5432"];
 $charset = 'utf8mb4';
 
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-$options = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES   => false,
-];
+$dsn = "pgsql:host=$host;port=$port;dbname=$db;charset=$charset";
 
 try {
-    $pdo = new PDO($dsn, $user, $pass, $options);
+    $pdo = new PDO($dsn, $user, $pass, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES => false,
+    ]);
+    echo "Connexion réussie à la base de données PostgreSQL !";
 } catch (PDOException $e) {
-    die('Erreur : ' . $e->getMessage());
+    echo 'Échec de la connexion : ' . $e->getMessage();
 }
 ?>
+
+
